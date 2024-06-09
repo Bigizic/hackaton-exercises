ee119061dc18d88f1c42868be7cf1eb516acf32a
Bigizic <olalekanisaac75@gmail.com>
2024-06-09 23:01:33 +0100

    started python solution to question 1

---
+++ question_1/py/solution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Solution to murder in the village
+    Time limit allowed = 1 sec
+"""
+
+"""
+murder_in_the_village - function that solves murder in the village problem
+
+@prams:
+    - n: number of suspects in the village {int}
+    - names: 
+
+Return:
+    - :
+"""
+def murder_in_the_village(n: int, names: str):
+    """Implementation
+    """
