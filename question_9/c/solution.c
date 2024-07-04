dd3f201fb318ea7e1bedcbf15c249e31fee73cb8
Bigizic <olalekanisaac75@gmail.com>
2024-07-05 00:09:59 +0100

    add update to c file

--- question_9/c/solution.c
+++ question_9/c/solution.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
 bool vagina_number_checker(int number, char *first, char *second);
+int vagina_main(int N, bool first_check, bool second_check, int count,
+                int result);
 
 /**
 * vagina_number_checker - function that checks if a number is a valid vagina number using recursion
@@ -16,8 +18,8 @@ bool vagina_number_checker(int number, char *first, char *second) {
     snprintf(str, sizeof(str), "%d", number);
     size_t length = strlen(str);
     if (length != 3) {
-        return false
-    }
+        return false;
+    };
     char *f = &str[0], *s = &str[1], *last = &str[2];
 
     if (first && second) {
@@ -36,7 +38,7 @@ bool vagina_number_checker(int number, char *first, char *second) {
 * @count: <int>
 * @result: <int>
 *
-* Return:
+* Return: smallest vagina number that is greater than or equal to N
  */
 int vagina_main(int N, bool first_check, bool second_check,
          int count, int result)
@@ -47,20 +49,34 @@ int vagina_main(int N, bool first_check, bool second_check,
         };
 
         if (!vagina_number_checker(N, NULL, NULL) && count == 0) {
-            return main(N + 1, NULL, NULL, count, result);
+            return vagina_main(N + 1, NULL, NULL, count, result);
         };
 
         if (!vagina_number_checker(N, NULL, NULL) && count > 0) {
-            return main(N + 1, first_check, NULL, count, result);
+            return vagina_main(N + 1, first_check, NULL, count, result);
         };
 
         if (vagina_number_checker(N, NULL, NULL) && count > 0) {
-            return main(N + 1, true, true, count + 1, result);
+            return vagina_main(N + 1, true, true, count + 1, result);
         };
 
         if (vagina_number_checker(N, NULL, NULL)) {
-            return main(N + 1, true, NULL, count + 1, N);
+            return vagina_main(N + 1, true, NULL, count + 1, N);
         };
+    };
+    return 0;
+}
+
+/**
+* main - entry point
+*
+* Return: 0
+ */
+int main(void) {
+    int numbers[5] = {320, 144, 516, 122, 100};
+
+    for (int i = 0; i < sizeof(numbers); i++) {
+        printf("%d\n", vagina_main(numbers[i], NULL, NULL, '\0', '\0'));
     };
     return 0;
 }
